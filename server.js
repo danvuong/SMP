1e19b15bc9e2f9d5d32914108f95d267f5c9e263
serge <merlin.boyer@wanadoo.fr>
2018-02-08 16:49:35 +0100

    server log

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 
-/*
-app.use(session());
-app.use(body());
-app.use(express.static('public'));
-*/
-
-//test2
 
 var clients = new Array();
 
@@ -33,39 +26,14 @@ io.on('connection', function(socket){
 	socket.on('login_required', function(data){
 		pseudo = data;
 		console.log('logged : ' + pseudo);
-		var tamere = 1;
-		for(i in clients){
-			if(clients[i].pseudo == pseudo){
-				socket.emit('mauvaisnombatard');
-				tamere = 0;
-			};
-		};
-		if(tamere == 1){socket.emit('logged');};
 	});
 
-	socket.on('deplacement', function(joueur){
-		if(joueur){
-			console.log(joueur.pseudo + 's est deplace');
-			io.emit('action', joueur);
-		}
-	})
 
-	socket.on('new_player', function(new_joueur){  /////////////
-		clients[new_joueur.pseudo] = new_joueur;
-		for(var i in clients){
-			console.log(clients[i].pseudo + ' send')
-			socket.emit('add_joueur', clients[i]);
-		};
-		socket.emit('go');
-		socket.broadcast.emit('add_joueur', new_joueur);
-	})
 
 	socket.on('disconnect', function(){
-		var k = clients.indexOf(pseudo);
-		clients.splice(k,1);
 		console.log('client : ' + pseudo + ' disconnected');
-		//console.log(clients[pseudo].pseudo);
 	});
+
 	
 	socket.on('print', function(msg){
 		console.log('mss : ' + msg);
