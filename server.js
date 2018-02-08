17109686fb3aea01f9a96b84374b6bfa47592b06
serge <merlin.boyer@wanadoo.fr>
2018-02-08 16:43:31 +0100

    aj server log

--- server.js
+++ server.js
@@ -33,38 +33,11 @@ io.on('connection', function(socket){
 	socket.on('login_required', function(data){
 		pseudo = data;
 		console.log('logged : ' + pseudo);
-		var tamere = 1;
-		for(i in clients){
-			if(clients[i].pseudo == pseudo){
-				socket.emit('mauvaisnombatard');
-				tamere = 0;
-			};
-		};
-		if(tamere == 1){socket.emit('logged');};
+		socket.emit('logged');
 	});
 
-	socket.on('deplacement', function(joueur){
-		if(joueur){
-			console.log(joueur.pseudo + 's est deplace');
-			io.emit('action', joueur);
-		}
-	})
-
-	socket.on('new_player', function(new_joueur){  /////////////
-		clients[new_joueur.pseudo] = new_joueur;
-		for(var i in clients){
-			console.log(clients[i].pseudo + ' send')
-			socket.emit('add_joueur', clients[i]);
-		};
-		socket.emit('go');
-		socket.broadcast.emit('add_joueur', new_joueur);
-	})
-
 	socket.on('disconnect', function(){
-		var k = clients.indexOf(pseudo);
-		clients.splice(k,1);
 		console.log('client : ' + pseudo + ' disconnected');
-		//console.log(clients[pseudo].pseudo);
 	});
 	
 	socket.on('print', function(msg){
